853b19afdfea81a127611d9bba3c8a0023119ffb
Hector0122 <hpave954@gmail.com>
2026-05-16 20:24:45 -0700

    fix: correct BASE_URL construction to handle optional port

--- api/server/index.ts
+++ api/server/index.ts
@@ -1,3 +1,4 @@
 import Config from 'react-native-config';
 
-export const BASE_URL = `${Config.BASE_URL || 'http://localhost'}:${Config.PORT || 3000}`;
+const port = Config.PORT ? `:${Config.PORT}` : ''
+export const BASE_URL = `${Config.BASE_URL || 'http://localhost'}${port}`;
